0b50cb6fcecf9ffa2e97e5c7184826be25e979b0
Mircea Salan <msalan@adobe.com>
2023-07-11 20:04:24 +0300

    Added style for footer header

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig, decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
@@ -9,7 +9,7 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // fetch footer content
-  const footerPath = cfg.footer || '/footer';
+  const footerPath = getMetadata('footer') || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
@@ -17,9 +17,36 @@ export default async function decorate(block) {
 
     // decorate footer DOM
     const footer = document.createElement('div');
+    footer.classList.add('footer-container');
     footer.innerHTML = html;
-
+    const footerHeader = footer.querySelector('div');
+    footerHeader.classList.add('footer-header');
+    wrapImgsInLinks(footerHeader);
+    wrapUnorderedListInDiv(footerHeader, 'ul-wrapper');
+    const footerHeaderLinks = footerHeader.querySelectorAll('a');
+    footerHeaderLinks.forEach(link => link.classList.add('footer-header-link'));
     decorateIcons(footer);
     block.append(footer);
   }
+}
+
+function wrapImgsInLinks(container) {
+  const pictures = container.querySelectorAll('picture');
+  pictures.forEach((pic) => {
+    const link = pic.nextElementSibling;
+    if (link && link.tagName === 'A' && link.href) {
+      link.innerHTML = pic.outerHTML;
+      pic.replaceWith(link);
+    }
+  });
+}
+
+function wrapUnorderedListInDiv(container, className) {
+  const unorderedLists = container.querySelectorAll('ul');
+  unorderedLists.forEach(ul => {
+    const div = document.createElement('div');
+    div.innerHTML = ul.outerHTML;
+    div.classList.add(className);
+    ul.replaceWith(div);
+  });
 }
