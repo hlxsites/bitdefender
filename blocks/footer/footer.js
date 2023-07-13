86bb4b8f8de833ed925f41517ffdf5722be5bfc7
Mircea Salan <msalan@adobe.com>
2023-07-13 11:47:14 +0300

    Changes to the footer display

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -25,6 +25,23 @@ export default async function decorate(block) {
     wrapUnorderedListInDiv(footerHeader, 'ul-wrapper');
     const footerHeaderLinks = footerHeader.querySelectorAll('a');
     footerHeaderLinks.forEach(link => link.classList.add('footer-header-link'));
+
+    const quickLinksSection = footerHeader.nextElementSibling;
+    quickLinksSection.classList.add('quick-list-section-container');
+    wrapParagraphAndULInDiv(quickLinksSection);
+
+
+    const chooseYourCountrySection = quickLinksSection.nextElementSibling;
+    chooseYourCountrySection.classList.add('choose-your-country-language-section');
+
+    const trustedSection = chooseYourCountrySection.nextElementSibling;
+    trustedSection.classList.add('trusted-section');
+
+    const copyrightSection = trustedSection.nextElementSibling;
+    copyrightSection.classList.add('copyright-section');
+    wrapUnorderedListInDiv(copyrightSection, 'copyright-ul-wrapper');
+    wrapParagraphInADiv(copyrightSection, 'copyright-section-p-wrapper');
+
     decorateIcons(footer);
     block.append(footer);
   }
@@ -41,12 +58,31 @@ function wrapImgsInLinks(container) {
   });
 }
 
-function wrapUnorderedListInDiv(container, className) {
+function wrapUnorderedListInDiv(container, divClassName) {
   const unorderedLists = container.querySelectorAll('ul');
   unorderedLists.forEach(ul => {
     const div = document.createElement('div');
     div.innerHTML = ul.outerHTML;
-    div.classList.add(className);
+    div.classList.add(divClassName);
     ul.replaceWith(div);
   });
+}
+
+function wrapParagraphAndULInDiv(container) {
+  const paragraphs = container.querySelectorAll('p');
+  paragraphs.forEach(p => {
+    const unorderedList = p.nextElementSibling;
+    const div = document.createElement('div');
+    div.innerHTML = `${p.outerHTML}${unorderedList.outerHTML}`;
+    p.replaceWith(div);
+    unorderedList.remove();
+  });
+}
+
+function wrapParagraphInADiv(container, divClassName) {
+  const paragraph = container.querySelector('p');
+  const div = document.createElement('div');
+  div.innerHTML = paragraph.outerHTML;
+  div.classList.add(divClassName);
+  paragraph.replaceWith(div);
 }
