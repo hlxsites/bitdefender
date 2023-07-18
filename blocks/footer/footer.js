eb80d37a937210e2a53da770ec51552501fc8f6c
Mircea Salan <msalan@adobe.com>
2023-07-18 14:30:39 +0300

    Build mobile footer mobile first

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,29 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig, decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
+
+function wrapImgsInLinks(container) {
+  const pictures = container.querySelectorAll('picture');
+  pictures.forEach((pic) => {
+    const link = pic.nextElementSibling;
+    if (link && link.tagName === 'A' && link.href) {
+      link.innerHTML = pic.outerHTML;
+      pic.replaceWith(link);
+    }
+  });
+}
+
+function onFooterElementClick(evt) {
+  evt.target.classList.toggle('active');
+}
+
+function disableSelectedCountry(container) {
+  const listOfCountries = container.querySelectorAll('li');
+  listOfCountries.forEach((countryLanguage) => {
+    if (countryLanguage.innerHTML.includes('selected')) {
+      countryLanguage.classList.add('deactivated');
+      countryLanguage.innerHTML = countryLanguage.innerHTML.replace('(selected)', '');
+    }
+  });
+}
 
 /**
  * loads and decorates the footer
@@ -9,7 +34,7 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // fetch footer content
-  const footerPath = cfg.footer || '/footer';
+  const footerPath = getMetadata('footer') || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
@@ -19,6 +44,14 @@ export default async function decorate(block) {
     const footer = document.createElement('div');
     footer.innerHTML = html;
 
+    wrapImgsInLinks(footer);
+    
+    const sectionHeaders = footer.querySelectorAll('div > div > p');
+    sectionHeaders[2].addEventListener('click', onFooterElementClick);
+    sectionHeaders[3].addEventListener('click', onFooterElementClick);
+
+    const sectionsData = footer.querySelectorAll('div > div > ul');
+    disableSelectedCountry(sectionsData[3]);
     decorateIcons(footer);
     block.append(footer);
   }
