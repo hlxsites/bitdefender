d0107d00825fc831b37977d9e56e303b13426db4
Vitaly Tsaplin <tsaplin@adobe.com>
2023-07-18 16:05:15 +0200

    fix: review feedback

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,15 +1,30 @@
 export default function decorate(block) {
-  block.querySelectorAll(':scope > div').forEach((item) => {
+  const items = Array.from(block.querySelectorAll(':scope > div'));
+  items.forEach((item) => {
     item.classList.add('accordion-item');
     const [header, content] = item.children;
     header.classList.add('accordion-item-header');
-    header.addEventListener('click', () => {
+    item.addEventListener('click', () => {
       if (!item.classList.contains('expanded')) {
+        items.filter((i) => i.classList.contains('expanded')).forEach((i) => {
+          i.classList.remove('expanded');
+          i.children[1].style.height = 0;
+        });
         content.style.height = `${content.scrollHeight}px`;
+        const transitionEndCallback = () => {
+          content.removeEventListener('transitionend', transitionEndCallback);
+          content.style.height = 'auto';
+          console.log('transitionend');
+        };
+        content.addEventListener('transitionend', transitionEndCallback);
+        item.classList.add('expanded');
       } else {
-        content.style.height = 0;
+        content.style.height = `${content.scrollHeight}px`;
+        requestAnimationFrame(() => {
+          item.classList.remove('expanded');
+          content.style.height = 0;
+        });
       }
-      item.classList.toggle('expanded');
     });
     content.classList.add('accordion-item-content');
   });
