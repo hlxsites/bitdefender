0899860903d7981f35e3ccc8c7e151a1636cd1f9
Nicolas Barbe <barbe@adobe.com>
2023-07-20 21:26:31 +0200

    highlight visible menu on scroll

--- blocks/sticky-nav/sticky-nav.js
+++ blocks/sticky-nav/sticky-nav.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 export default function decorate(block) {
   const menu = block.querySelector('ul');
   menu.classList.add('menu');
@@ -7,34 +8,60 @@ export default function decorate(block) {
 
   const menuEntries = block.querySelectorAll('ul > li');
 
-  const mobileMenu = document.createElement('ul');
-  mobileMenu.className = 'mobile-menu';
-  mobileMenu.appendChild(menuEntries[0].cloneNode(true));
-  block.prepend(mobileMenu);
+  const mobileDropDown = document.createElement('div');
+  mobileDropDown.className = 'mobile-dropdown';
+  mobileDropDown.innerText = menuEntries[0].innerText;
+  block.prepend(mobileDropDown);
 
-  /* listen to click event to visually select the menu item in the nav */
-  menuEntries.forEach((li) => li.addEventListener('click', () => {
-    menuEntries.forEach((menuItem) => {
-      menuItem.classList.remove('active');
-    });
-    li.classList.add('active');
-    mobileMenu.replaceChildren(li.cloneNode(true));
-    mobileMenu.classList.remove('opened');
-  }));
-
-  /* listen to click event to open or close the menu on mobile */
-  mobileMenu.addEventListener('click', (e) => {
+  /* listen to click event to open or close the dropdown menu on mobile */
+  mobileDropDown.addEventListener('click', (e) => {
     e.preventDefault();
-    mobileMenu.classList.toggle('opened');
+    mobileDropDown.classList.toggle('opened');
   });
 
-  /* listen to scroll event to stick the nav on the top when necessary */
+  menu.addEventListener('click', () => {
+    mobileDropDown.classList.toggle('opened');
+  });
+
+  /* listen to scroll event to stick the nav on the top and update the current visible section */
   document.addEventListener('scroll', () => {
+    // stick the navigation on the top
     const wrapperTop = block.parentElement.offsetTop;
     if (window.scrollY >= wrapperTop) {
       block.classList.add('fixed-nav');
     } else {
       block.classList.remove('fixed-nav');
+    }
+
+    // update the active heading based on scroll position
+    const headings = document.querySelectorAll('h2');
+
+    const visibleHeading = Array.from(headings).reduce((visHeading, curHeading) => {
+      const curTop = curHeading.getBoundingClientRect().top;
+      // return current heading if at max 115 px from the top of the screen
+      // 115 is a magic number working with all size of screens
+      if (curTop < 115) {
+        return curHeading;
+      }
+      return visHeading;
+    }, null);
+
+    if (visibleHeading) {
+      const previousActiveMenuEntry = menu.querySelector('li.active');
+      const visibleActiveMenuEntryLink = menu.querySelector(`li a[href="#${visibleHeading.id}"]`);
+
+      // the heading is present in the sticky menu
+      if (visibleActiveMenuEntryLink) {
+        const visibleActiveMenuEntry = visibleActiveMenuEntryLink.parentElement;
+        visibleActiveMenuEntry.classList.add('active');
+        mobileDropDown.innerText = visibleActiveMenuEntry.innerText;
+
+        if (previousActiveMenuEntry && !previousActiveMenuEntry.isSameNode(visibleActiveMenuEntry)) {
+          previousActiveMenuEntry.classList.remove('active');
+        }
+      } else if (previousActiveMenuEntry) {
+        previousActiveMenuEntry.classList.remove('active');
+      }
     }
   });
 }
--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -563,38 +563,33 @@ export function decorateButtons(element) {
     : `<span class="button-text">${a.textContent}</span>${a.querySelector('span.icon')?.outerHTML || ''}`
   );
   element.querySelectorAll('a').forEach((a) => {
-    // Suppress a-to-button decoration when in excludedParentClasses
-    const excludedParentClasses = ['.sticky-nav'];
-    const isClosest = (el) => a.closest(el);
-    if (!excludedParentClasses.some(isClosest)) {
-      a.title = a.title || a.textContent;
-      if (a.href !== a.textContent) {
-        const up = a.parentElement;
-        const twoup = a.parentElement.parentElement;
-        if (!a.querySelector('img')) {
-          // Example: <p><strong><a href="example.com">Text</a></strong></p>
-          if (up.childNodes.length === 1 && up.tagName === 'STRONG'
-            && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-            a.className = 'button primary';
-            twoup.classList.add('button-container');
-            up.replaceWith(a);
-            a.innerHTML = wrapButtonText(a);
-            return;
-          }
-          // Example: <p><a href="example.com">Text</a> (example.com)</p>
-          if (up.childNodes.length === 2 && up.tagName === 'P' && a.nextSibling?.textContent.trim().startsWith('(')) {
-            a.className = 'button modal';
-            up.classList.add('button-container');
-            a.dataset.modal = a.nextSibling.textContent.trim().slice(1, -1);
-            a.nextSibling.remove();
-            return;
-          }
-          // Example: <p><a href="example.com">Text</a></p>
-          if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-            a.className = 'button'; // default
-            up.classList.add('button-container');
-            a.innerHTML = wrapButtonText(a);
-          }
+    a.title = a.title || a.textContent;
+    if (a.href !== a.textContent) {
+      const up = a.parentElement;
+      const twoup = a.parentElement.parentElement;
+      if (!a.querySelector('img')) {
+        // Example: <p><strong><a href="example.com">Text</a></strong></p>
+        if (up.childNodes.length === 1 && up.tagName === 'STRONG'
+          && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
+          a.className = 'button primary';
+          twoup.classList.add('button-container');
+          up.replaceWith(a);
+          a.innerHTML = wrapButtonText(a);
+          return;
+        }
+        // Example: <p><a href="example.com">Text</a> (example.com)</p>
+        if (up.childNodes.length === 2 && up.tagName === 'P' && a.nextSibling?.textContent.trim().startsWith('(')) {
+          a.className = 'button modal';
+          up.classList.add('button-container');
+          a.dataset.modal = a.nextSibling.textContent.trim().slice(1, -1);
+          a.nextSibling.remove();
+          return;
+        }
+        // Example: <p><a href="example.com">Text</a></p>
+        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
+          a.className = 'button'; // default
+          up.classList.add('button-container');
+          a.innerHTML = wrapButtonText(a);
         }
       }
     }
