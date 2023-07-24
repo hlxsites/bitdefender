d9c839622b556eb49c6073695515da09b4eb4449
ndere <ndere@adobe.com>
2023-07-24 09:28:03 +0200

    dedicated barchart block implemented

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -15,5 +15,4 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
-
 }
