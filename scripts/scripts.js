3121220d82efcbf5f81b76597aa886414bc5908d
Andrei Bogdan <andbogda@adobe.com>
2023-07-25 19:10:06 +0300

    Refactor buildHero

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,8 +12,6 @@ import {
   loadCSS,
 } from './lib-franklin.js';
 
-import { buildHeroBlock } from '../blocks/hero/hero.js';
-
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
@@ -42,19 +40,6 @@ export function setLanguage(pathname) {
 }
 
 /**
- * Builds all synthetic blocks in a container element.
- * @param {Element} main The container element
- */
-function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
-}
-
-/**
  * Decorates the main element.
  * @param {Element} main The main element
  */
@@ -63,7 +48,6 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
-  buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
 }
