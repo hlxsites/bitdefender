a6ab9b4cf0d9e1d6ffc46a07670db9139ac7b91b
Johan Trieloff <johan@trieloff.net>
2023-07-25 15:20:55 +0200

    carousel func

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,32 +1,44 @@
-function turnH3intoP() {
-  const h3 = document.querySelectorAll('h3');
-  h3.forEach((h3) => {
-    const p = document.createElement('p');
-    p.innerHTML = h3.innerHTML;
-    h3.parentNode.replaceChild(p, h3);
+function createDivider() {
+  const paragraphs = document.querySelectorAll('.carousel.block > div > div > p:first-child');
+
+  paragraphs.forEach(p => {
+    const divider = document.createElement('hr');
+    p.after(divider);
   });
 }
 
-function wrapCarousel() {
-  const carouselBlock = document.querySelector('.carousel.block');
-  const carouselContainer = document.createElement('div');
-  carouselContainer.className = 'carousel-container';
+function createNextButtons() {
+  const carousel = document.querySelector('.carousel.block');
+  const carouselItems = carousel.children;
+  const numOfChildren = carouselItems.length;
 
-  const childDivs = carouselBlock.querySelectorAll(':scope > div');
+  const titleDescription = document.querySelectorAll('.carousel.block > div > div > p:last-child');
+  titleDescription.forEach((element, index) => {
+    const nextButtons = document.createElement('div');
+    nextButtons.classList.add('next-buttons');
+    element.after(nextButtons);
 
-  if (childDivs.length >= 4) {
-    // Get the last four divs
-    const lastFourDivs = Array.from(childDivs).slice(-4);
+    for (let i = 0; i < numOfChildren; i++) {
+      const circle = document.createElement('span');
+      circle.classList.add('circle');
+      if (i === index) {
+        circle.classList.add('active'); // set the circle of the current carousel item as active
+      }
+      circle.addEventListener('click', () => {
+        document.querySelector('.carousel.block > div.active').classList.remove('active'); // hide the previously visible carousel item
+        carouselItems[i].classList.add('active'); // show the selected carousel item
+        document.querySelector('.circle.active').classList.remove('active'); // deactivate the previously active circle
+        circle.classList.add('active'); // activate the clicked circle
+      });
+      nextButtons.appendChild(circle);
+    }
+  });
 
-    lastFourDivs.forEach((div) => {
-      carouselContainer.appendChild(div);
-    });
-
-    carouselBlock.appendChild(carouselContainer);
-  }
+  // make the first carousel item visible by default
+  carouselItems[0].classList.add('active');
 }
 
-export default function decorate(block) {
-  turnH3intoP();
-  wrapCarousel();
+export default function decorate() {
+  createDivider();
+  createNextButtons();
 }
