400d1ad5d3e1a2be5047073b7a30642401f8e1ab
Vitaly Tsaplin <tsaplin@adobe.com>
2023-07-26 17:09:05 +0200

    feat: add content groups

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -1,5 +1,25 @@
+function createContainer(parent, elements) {
+  const container = document.createElement('div');
+  container.classList.add('product-card-group');
+  elements.forEach((g) => container.append(g));
+  parent.append(container);
+}
+
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    row.classList.add('product-card');
+  [...block.children].forEach((card) => {
+    card.classList.add('product-card');
+    let elements = [];
+    [...(card.firstElementChild.children)].forEach((child) => {
+      if (child.tagName === 'HR') {
+        createContainer(card.firstElementChild, elements);
+        elements = [];
+      } else {
+        elements.push(child);
+      }
+      child.remove();
+    });
+    if (elements.length) {
+      createContainer(card.firstElementChild, elements);
+    }
   });
 }
