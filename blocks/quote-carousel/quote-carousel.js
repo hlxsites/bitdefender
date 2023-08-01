27baf16fd2e693a8acd315dec77da4dcb6077a2f
Raul Hudea <rhudea@adobe.com>
2023-08-02 00:06:36 +0300

    styling and accessibility

--- blocks/quote-carousel/quote-carousel.js
+++ blocks/quote-carousel/quote-carousel.js
@@ -1,7 +1,7 @@
 import { createTag } from '../../scripts/utils/utils.js';
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 
-function createSlide(item) {
+function createSlide(item, index) {
   const quote = item.querySelector('p:not(:has(> strong, > em)):not(:empty)');
   const author = item.querySelector('p > strong');
   const description = item.querySelector('p > em');
@@ -10,7 +10,14 @@ function createSlide(item) {
   }
   return createTag(
     'div',
-    { class: 'slide' },
+    {
+      class: 'slide',
+      id: `carousel-${index}`,
+      role: 'tabpanel',
+      'aria-hidden': 'true',
+      'aria-described-by': `carousel-control-${index}`,
+      tabindex: '-1',
+    },
     `<div class="quote"><span class="icon icon-dark-blue-quote"/></div><div class="quote-content">
 <h4>${quote?.textContent}</h4>
 <h5>${author?.textContent}</h5>
@@ -25,8 +32,20 @@ function getCurrentSlideIndex(slides) {
 
 function updateControls(block, nextIndex) {
   const dots = block.querySelector('.slides-dots');
-  dots.querySelector('.active')?.classList.remove('active');
-  dots.children[nextIndex]?.classList.add('active');
+  const currentActive = dots.querySelector('.active');
+  if (currentActive) {
+    currentActive.classList.remove('active');
+    const btn = currentActive.querySelector('button');
+    btn.removeAttribute('aria-selected');
+    btn.setAttribute('tabindex', '-1');
+  }
+  const nextActive = dots.children[nextIndex];
+  if (nextActive) {
+    nextActive.classList.add('active');
+    const btn = nextActive.querySelector('button');
+    btn.setAttribute('aria-selected', 'true');
+    btn.setAttribute('tabindex', '0');
+  }
 }
 
 function updateSlide(nextIndex, block) {
@@ -34,8 +53,17 @@ function updateSlide(nextIndex, block) {
   const currentIndex = getCurrentSlideIndex(slides);
   slides.children[currentIndex].classList.remove('active');
   slides.children[nextIndex].classList.add('active');
-  const translateX = 100;
-  slides.style.transform = `translateX( ${-nextIndex * translateX}% )`;
+  const translateX = -nextIndex * 100;
+  slides.style.transform = `translateX( ${translateX}% )`;
+  [...slides.children].forEach((slide, index) => {
+    if (index === nextIndex) {
+      slide.removeAttribute('tabindex');
+      slide.setAttribute('aria-hidden', 'false');
+    } else {
+      slide.setAttribute('tabindex', '-1');
+      slide.setAttribute('aria-hidden', 'true');
+    }
+  });
   updateControls(block, nextIndex);
 }
 
@@ -48,23 +76,46 @@ function addDotsListeners(dotsControls, slides) {
   });
 }
 
+function createDotsControls(slides, block) {
+  const dots = createTag('ul', { class: 'slides-dots' });
+  const slidesNumber = slides.children.length;
+  [...slides.children].forEach((slide, slideIndex) => {
+    const ariaIndex = slideIndex + 1;
+    const btn = createTag(
+      'button',
+      {
+        id: `carousel-control-${ariaIndex}`,
+        class: 'dot',
+        role: 'tab',
+        type: 'button',
+        tabindex: '-1',
+        'aria-controls': `carousel-slide-${ariaIndex}`,
+        'aria-label': `${ariaIndex} of ${slidesNumber}`,
+      },
+    );
+    const li = createTag('li', { role: 'presentation' }, btn);
+    li.addEventListener('click', () => {
+      updateSlide(slideIndex, block);
+    });
+    dots.append(li);
+  });
+  return dots;
+}
+
 export default async function decorate(block) {
   const slides = createTag('div', { class: 'slides' });
+  let slideIndex = 1;
   [...block.children]
-    .map((item) => createSlide(item))
+    .map((item) => createSlide(item, slideIndex))
     .filter((item) => item)
     .forEach((item) => {
+      slideIndex += 1;
       slides.append(item);
     });
   slides.children[0].classList.add('active');
-  const dotsControls = createTag('ul', { class: 'slides-dots' });
-  [...slides.children].forEach((slide, slideIndex) => {
-    const btn = createTag('li', {}, '<button class="dot"></button>');
-    btn.addEventListener('click', () => {
-      updateSlide(slideIndex, block);
-    });
-    dotsControls.append(btn);
-  });
+
+  const dotsControls = createDotsControls(slides, block);
+
   /* Add carousel action button if it exists */
   const controlsElements = [dotsControls];
   const button = block.querySelector('.button-container');
