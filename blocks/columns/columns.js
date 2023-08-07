dc83c757da5fdcd77a34ba5380ad416531722dc0
Johan Trieloff <johan@trieloff.net>
2023-08-07 15:40:52 +0200

    new carousel

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,38 +1,66 @@
-function setupCarousel(carousel) {
-  // Use the provided CSS selector to get all columns that need to be in the carousel
-  const items = Array.from(carousel.querySelectorAll('.columns.carousel > div > div.columns-img-col:nth-child(n+4), .columns.carousel > div:nth-child(2) > div:nth-child(n+4)'));
-  let currentIndex = 0;
+function countSlides(carouselContent) {
+  const numberOfItems = carouselContent.children.length;
+  return Math.ceil(numberOfItems / 3);
+}
 
-  function showItemsFromIndex(index) {
-    // Hide all items first
-    items.forEach(item => item.style.display = 'none');
+function hideExcessElements() {
+  const carousel = document.querySelector('.columns.carousel');
+  const carouselContentImage = carousel.querySelector('.columns.carousel > div:nth-child(1)');
+  const carouselContentText = carousel.querySelector('.columns.carousel > div:nth-child(2)');
 
-    // Show the next three items
-    for (let i = index; i < (index + 3) && i < items.length; i++) {
-      items[i].style.display = 'block';
-    }
+  // Hide excess images
+  const childImageDivs = carouselContentImage.querySelectorAll('.columns-img-col');
+  for (let i = 3; i < childImageDivs.length; i++) {
+    childImageDivs[i].style.display = 'none';
   }
 
-  const nextButton = document.createElement('button');
-  nextButton.addEventListener('click', function () {
-    currentIndex = (currentIndex + 3) % items.length;
-    showItemsFromIndex(currentIndex);
+  // Hide excess texts
+  const childTextDivs = carouselContentText.querySelectorAll('div');
+  for (let i = 3; i < childTextDivs.length; i++) {
+    childTextDivs[i].style.display = 'none';
+  }
+}
+
+function setActiveButton(button, buttonsWrapper) {
+  // Remove active class from all buttons
+  buttonsWrapper.querySelectorAll('button').forEach((btn) => {
+    btn.classList.remove('active');
   });
 
-  const prevButton = document.createElement('button');
-  prevButton.addEventListener('click', function () {
-    currentIndex -= 3;
-    if (currentIndex < 0) {
-      currentIndex = items.length - 3;
-    }
-    showItemsFromIndex(currentIndex);
-  });
+  button.classList.add('active');
+}
 
-  carousel.appendChild(prevButton);
-  carousel.appendChild(nextButton);
+function createNavigationButtons(numberOfSlides) {
+  const buttonsWrapper = document.createElement('div');
+  buttonsWrapper.className = 'carousel-buttons';
 
-  // Initially show the first three items
-  showItemsFromIndex(currentIndex);
+  for (let i = 0; i < numberOfSlides; i++) {
+    const button = document.createElement('button');
+    button.addEventListener('click', function() {
+      setActiveButton(button, buttonsWrapper);
+    });
+    buttonsWrapper.appendChild(button);
+  }
+
+  // By default, set the first button as the active button
+  if (buttonsWrapper.firstChild) {
+    buttonsWrapper.firstChild.classList.add('active');
+  }
+
+  return buttonsWrapper;
+}
+
+function setupCarousel(carousel) {
+  const carouselContent = carousel.querySelector('.columns.carousel > div');
+
+  // Determine the number of slides
+  const numberOfSlides = countSlides(carouselContent);
+  // Generate navigation buttons for the carousel
+  const buttonsWrapper = createNavigationButtons(numberOfSlides);
+
+  carousel.appendChild(buttonsWrapper);
+
+  hideExcessElements();
 }
 
 export default function decorate(block) {
