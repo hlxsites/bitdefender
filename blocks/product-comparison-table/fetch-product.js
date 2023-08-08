cebf5df3f57f73a53981a6d979f4980ada7205f5
Mircea Salan <salan.mircea@gmail.com>
2023-08-08 16:30:30 +0300

    Fix js linting issues

--- blocks/product-comparison-table/fetch-product.js
+++ blocks/product-comparison-table/fetch-product.js
@@ -25,7 +25,7 @@ export default async function fetchProduct(code = 'av', variant = '1u-1y') {
           pid: null,
         },
       },
-    })
+    }),
   );
 
   const res = await fetch(FETCH_URL, {
