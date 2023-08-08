bd9461479bcb85a1e3a615f307c467107c8ff06b
Mircea Salan <salan.mircea@gmail.com>
2023-08-08 16:29:04 +0300

    Fix js linting issues

--- blocks/product-comparison-table/fetch-product.js
+++ blocks/product-comparison-table/fetch-product.js
@@ -1,11 +1,11 @@
 const FETCH_URL = 'https://www.bitdefender.com/site/Store/ajax';
 
 export const PRODUCTS = {
-    "Bitdefender Mobile Security": "mobile",
-    "Bitdefender Total Security": "tsmd",
-    "Bitdefender Internet Security": "is",
-    "Bitdefender Antivirus Plus": "av"
-}
+  'Bitdefender Mobile Security': 'mobile',
+  'Bitdefender Total Security': 'tsmd',
+  'Bitdefender Internet Security': 'is',
+  'Bitdefender Antivirus Plus': 'av',
+};
 
 /**
  * Fetches a product from the Bitdefender store.
@@ -14,8 +14,10 @@ export const PRODUCTS = {
  * @returns {Promise<*>}
  */
 export default async function fetchProduct(code = 'av', variant = '1u-1y') {
-    const data = new FormData();
-    data.append('data', JSON.stringify({
+  const data = new FormData();
+  data.append(
+    'data',
+    JSON.stringify({
       ev: 1,
       product_id: code,
       config: {
@@ -23,28 +25,28 @@ export default async function fetchProduct(code = 'av', variant = '1u-1y') {
           pid: null,
         },
       },
-    }));
-  
-    const res = await fetch(FETCH_URL, {
-      method: 'POST',
-      body: data,
-    });
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
-    const json = await res.json();
-  
+    })
+  );
+
+  const res = await fetch(FETCH_URL, {
+    method: 'POST',
+    body: data,
+  });
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+  const json = await res.json();
+
+  // eslint-disable-next-line guard-for-in,no-restricted-syntax
+  for (const i in json.data.product.variations) {
     // eslint-disable-next-line guard-for-in,no-restricted-syntax
-    for (const i in json.data.product.variations) {
-      // eslint-disable-next-line guard-for-in,no-restricted-syntax
-      for (const j in json.data.product.variations[i]) {
-        const v = json.data.product.variations[i][j];
-        if (v.variation.variation_name === variant) {
-          return v;
-        }
+    for (const j in json.data.product.variations[i]) {
+      const v = json.data.product.variations[i][j];
+      if (v.variation.variation_name === variant) {
+        return v;
       }
     }
-  
-    throw new Error('Variant not found');
   }
-  
+
+  throw new Error('Variant not found');
+}
