13bc1bcb2416595161de473e076f307dc19b0747
sergiucoman <sergiu.coman@gmail.com>
2023-08-09 15:51:52 +0300

    Columns with tabular data (#91)
    Co-authored-by: scoman <scoman@adobe.com>

---
+++ blocks/features/features.js
@@ -0,0 +1,124 @@
+import {
+  decorateTags,
+} from '../../scripts/scripts.js';
+
+function expandItem(content) {
+  content.style.height = `${content.scrollHeight}px`;
+  const transitionEndCallback = () => {
+    content.removeEventListener('transitionend', transitionEndCallback);
+    content.style.height = 'auto';
+  };
+  content.addEventListener('transitionend', transitionEndCallback);
+  content.classList.add('expanded');
+}
+
+function collapseItem(content) {
+  content.style.height = `${content.scrollHeight}px`;
+  requestAnimationFrame(() => {
+    content.classList.remove('expanded');
+    content.style.height = 0;
+  });
+}
+
+function eventListener(ul) {
+  return (event) => {
+    // if the clicked node is not open then open it
+    if (!event.target.classList.contains('is-open')) {
+      event.target.classList.add('is-open');
+
+      // if the clicked node has children then toggle the expanded class
+      if (event.target.parentNode.children.length > 1) {
+        event.target.parentNode.querySelectorAll('.features-tabs-content').forEach((content) => {
+          expandItem(content);
+        });
+      }
+
+      // hid the other tabs
+      ul.querySelectorAll('li').forEach((collapsedLi) => {
+        if (collapsedLi !== event.target.parentNode) {
+          collapsedLi.children[0].classList.remove('is-open');
+          collapsedLi.querySelectorAll('.features-tabs-content').forEach((content) => {
+            collapseItem(content);
+          });
+        }
+      });
+    } else {
+      event.target.classList.remove('is-open');
+      // if the clicked node has children then toggle the expanded class
+      if (event.target.parentNode.children.length > 1) {
+        event.target.parentNode.querySelectorAll('.features-tabs-content').forEach((content) => {
+          collapseItem(content);
+        });
+      }
+    }
+  };
+}
+
+function extractFeatures(col) {
+  const ul = document.createElement('ul');
+  ul.classList.add('features-tabs');
+
+  // select all h4 tags as feature titles
+  col.querySelectorAll('h4').forEach((h4) => {
+    const li = document.createElement('li');
+    ul.appendChild(li);
+
+    const a = document.createElement('a');
+
+    // register click event on a tag
+    a.addEventListener('click', eventListener(ul));
+
+    h4.childNodes.forEach((node) => {
+      if (node.nodeType === Node.TEXT_NODE) {
+        a.appendChild(document.createTextNode(node.textContent));
+      } else if (node !== li.lastChild || node.tagName !== 'SPAN') {
+        a.appendChild(node);
+      }
+    });
+
+    a.classList.add('features-tabs-title');
+
+    li.appendChild(a);
+
+    const content = document.createElement('div');
+    content.classList.add('features-tabs-content');
+    li.appendChild(content);
+
+    // every oaragraph until next h4
+    let nextElement = h4.nextElementSibling;
+    while (nextElement && nextElement.tagName !== 'H4') {
+      content.appendChild(nextElement);
+      nextElement = h4.nextElementSibling;
+    }
+
+    ul.appendChild(li);
+
+    h4.remove();
+  });
+
+  return ul;
+}
+
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`features-${cols.length}-cols`);
+
+  decorateTags(block);
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('features-img-col');
+        }
+      }
+
+      // setup tabs
+      col.appendChild(extractFeatures(col));
+    });
+  });
+}
