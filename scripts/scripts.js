e550aa8a52d4bc06d8cf8aa01eee42b688c9bc4f
Andrei Bogdan <andbogda@adobe.com>
2023-08-17 16:16:14 +0300

    Add back breadcrumb

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -256,7 +256,7 @@ function loadDelayed() {
   window.setTimeout(() => {
     // eslint-disable-next-line import/no-cycle
     import('./delayed.js');
-    // import('./breadcrumbs.js');
+    import('./breadcrumbs.js');
   }, 3000);
   // load anything that can be postponed to the latest here
 }
