4bcce372e4e1d7dad1963b7a5983f3a6fe2dd0c9
Tobias Reiss <reiss@adobe.com>
2023-09-04 12:47:10 +0200

    feat: page loaded event & started event earlier

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,7 +3,6 @@ import {
   loadScript,
   sampleRUM,
   fetchPlaceholders,
-  getMetadata,
 } from './lib-franklin.js';
 
 // eslint-disable-next-line import/no-cycle
@@ -11,103 +10,16 @@ import {
   getLanguageCountryFromPath,
   pushProductsToDataLayer,
   pushToDataLayer,
-  getTags,
-  getOperatingSystem,
-  METADATA_ANAYTICS_TAGS,
+  getEnvironment,
 } from './scripts.js';
 import { loadBreadcrumbs } from './breadcrumbs.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-/**
- * Returns the environment name based on the hostname
- * @returns {String}
- */
-function getEnvironment(hostname, country) {
-  if (hostname.includes('hlx.page') || hostname.includes('hlx.live')) {
-    return 'stage';
-  }
-  if (hostname.includes(`.${country}`)) {
-    return 'prod';
-  }
-  return 'dev';
-}
-
-const PATHNAME = window.location.pathname;
-const HOSTNAME = window.location.hostname;
-const LANGUAGE_COUNTRY = getLanguageCountryFromPath(PATHNAME);
+const LANGUAGE_COUNTRY = getLanguageCountryFromPath(window.location.pathname);
 const LAUNCH_URL = 'https://assets.adobedtm.com';
-const ENVIRONMENT = getEnvironment(HOSTNAME, LANGUAGE_COUNTRY.country);
-
-/**
- * Returns the current user operating system based on userAgent
- * @returns {String}
- */
-/**
- * Returns the value of a query parameter
- * @returns {String}
- */
-function getParamValue(param) {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(param);
-}
-
-/**
- * Returns the current user time in the format HH:MM|HH:00-HH:59|dayOfWeek|timezone
- * @returns {String}
- */
-function getCurrentTime() {
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const dayOfWeek = date.getDay();
-  const timezone = date.getTimezoneOffset();
-  return `${hours}:${minutes}|${hours}:00-${hours}:59|${dayOfWeek}|${timezone}`;
-}
-
-/**
- * Returns the current GMT date in the format DD/MM/YYYY
- * @returns {String}
- */
-function getCurrentDate() {
-  const date = new Date();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
-}
-
-function pushPageLoadToDataLayer() {
-  const tags = getTags(getMetadata(METADATA_ANAYTICS_TAGS));
-  pushToDataLayer('page load started', {
-    pageInstanceID: ENVIRONMENT,
-    page: {
-      info: {
-        name: [LANGUAGE_COUNTRY.country, ...tags].join(':'), // e.g. au:consumer:product:internet security
-        section: LANGUAGE_COUNTRY.country || '',
-        subSection: tags[0] || '',
-        subSubSection: tags[1] || '',
-        subSubSubSection: tags[2] || '',
-        destinationURL: window.location.href,
-        queryString: window.location.search,
-        referringURL: getParamValue('ref') || getParamValue('adobe_mc') || document.referrer || '',
-        serverName: 'hlx.live', // indicator for AEM Success Edge
-        language: navigator.language || navigator.userLanguage || LANGUAGE_COUNTRY.language,
-        sysEnv: getOperatingSystem(window.navigator.userAgent),
-      },
-      attributes: {
-        promotionID: getParamValue('pid') || '',
-        internalPromotionID: getParamValue('icid') || '',
-        trackingID: getParamValue('cid') || '',
-        time: getCurrentTime(),
-        date: getCurrentDate(),
-        domain: HOSTNAME,
-        domainPeriod: HOSTNAME.split('.').length,
-      },
-    },
-  });
-}
+const ENVIRONMENT = getEnvironment(window.location.hostname, LANGUAGE_COUNTRY.country);
 
 // Load Adobe Experience platform data collection (Launch) script
 const { launchProdScript, launchStageScript, launchDevScript } = await fetchPlaceholders();
@@ -120,8 +32,8 @@ switch (ENVIRONMENT) {
     loadScript(LAUNCH_URL + launchDevScript); break;
 }
 
-pushPageLoadToDataLayer();
 pushProductsToDataLayer();
+pushToDataLayer('page loaded');
 
 // Load breadcrumbs
 loadBreadcrumbs();
