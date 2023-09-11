8b876c14110d17540dacc403a0630336c2f488c4
Andrei Bogdan <andbogda@adobe.com>
2023-09-11 10:57:34 +0300

    Remove commented code

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -19,13 +19,6 @@ function buildHeroBlock(element) {
     const section = document.querySelector('div.hero');
     const subSection = document.querySelector('div.hero div');
     subSection.classList.add('hero-content');
-    // get number of children in hero-content
-    // const numberOfChildren = subSection.childElementCount;
-
-    // iterate though children and add numbered class to each child
-    // for (let i = 0; i < numberOfChildren; i += 1) {
-    //   subSection.children[i].classList.add(`hero-content-${i}`);
-    // }
 
     const breadcrumb = createTag('div', { class: 'breadcrumb' });
     document.querySelector('div.hero div div:first-child').prepend(breadcrumb);
