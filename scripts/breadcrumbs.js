1c6af7efadf2fdf6b1a913e17c0764c3a30a8055
Mircea Salan <salan.mircea@gmail.com>
2023-10-03 11:27:09 +0300

    remove breadcrumb check because is introduced in other PR

--- scripts/breadcrumbs.js
+++ scripts/breadcrumbs.js
@@ -86,7 +86,7 @@ export async function loadBreadcrumbs() {
   decorateBlockWithRegionId(breadcrumb, 'Hero|Breadcrumb');
 
   // check if breadcrumb div exists
-  if (breadcrumb !== undefined && breadcrumb !== null) {
+  if (breadcrumb !== undefined) {
     await createBreadcrumbs(breadcrumb);
   }
 }
