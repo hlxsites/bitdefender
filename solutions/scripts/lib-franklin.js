c6c30a0c4d31eeab1e2219b6c6b83b56c62a8480
ext-vradulescu-bf <140399829+ext-vradulescu-bf@users.noreply.github.com>
2023-10-30 15:41:54 +0200

    Update lib-franklin.js

--- solutions/scripts/lib-franklin.js
+++ solutions/scripts/lib-franklin.js
@@ -735,13 +735,13 @@ export function loadFooter(footer) {
 export function setup() {
   window.hlx = window.hlx || {};
   window.hlx.RUM_MASK_URL = 'full';
-  window.hlx.codeBasePath = `/${window.location.pathname.split('/')[1]}`;
+  window.hlx.codeBasePath = '';
   window.hlx.lighthouse = new URLSearchParams(window.location.search).get('lighthouse') === 'on';
 
   const scriptEl = document.querySelector('script[src$="/solutions/scripts/scripts.js"]');
   if (scriptEl) {
     try {
-      window.hlx.codeBasePath = window.hlx.codeBasePath + new URL(scriptEl.src).pathname.split('/scripts/scripts.js')[0];
+      [window.hlx.codeBasePath] = new URL(scriptEl.src).pathname.split('/scripts/scripts.js');
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
