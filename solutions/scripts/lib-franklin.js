456e000ee314aa0bde287c7814f9252d8ec071da
ext-vradulescu-bf <140399829+ext-vradulescu-bf@users.noreply.github.com>
2023-10-30 15:24:24 +0200

    Update lib-franklin.js url match

--- solutions/scripts/lib-franklin.js
+++ solutions/scripts/lib-franklin.js
@@ -735,13 +735,13 @@ export function loadFooter(footer) {
 export function setup() {
   window.hlx = window.hlx || {};
   window.hlx.RUM_MASK_URL = 'full';
-  window.hlx.codeBasePath = '';
+  window.hlx.codeBasePathp = `/${window.location.pathname.split('/')[1]}`;
   window.hlx.lighthouse = new URLSearchParams(window.location.search).get('lighthouse') === 'on';
 
   const scriptEl = document.querySelector('script[src$="/solutions/scripts/scripts.js"]');
   if (scriptEl) {
     try {
-      [window.hlx.codeBasePath] = new URL(scriptEl.src).pathname.split('/scripts/scripts.js');
+      window.hlx.codeBasePath = window.hlx.codeBasePath + new URL(scriptEl.src).pathname.split('/scripts/scripts.js')[0];
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
