fb087f00e07c4aaa0bb3c2b3b76bbd74cabd1331
Buga Adrian Alexandru <abuga@bitdefender.com>
2023-12-13 17:39:45 +0200

    .7

--- _src/blocks/hero-aem/hero-aem.js
+++ _src/blocks/hero-aem/hero-aem.js
@@ -100,7 +100,7 @@ export default function decorate(block, options) {
     decorateBuyLink(buyLink, onSelectorClass);
 
     const pricesBox = createPricesElement(options.store, conditionText, saveText, prodName, prodUsers, prodYears);
-    buyLink.parentNode.parentNode.insertBefore(pricesBox, buyLink.parentNode);
+    richText.appendChild(pricesBox);
   }
 
   window.dispatchEvent(new CustomEvent('shadowDomLoaded'), {
