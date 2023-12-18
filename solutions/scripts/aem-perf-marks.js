620139ca684972d228b8b0b0e1f9dd1127f68b2a
Vitaly Tsaplin <tsaplin@adobe.com>
2023-12-18 12:38:07 +0100

    feat: improve performance measuring

--- solutions/scripts/aem-perf-marks.js
+++ solutions/scripts/aem-perf-marks.js
@@ -10,11 +10,6 @@ window.PerfMarks = window.PerfMarks || {};
  * @param {any} detail The detail to pass to the performance mark.
  */
 window.PerfMarks.create = (name, detail = undefined) => {
-  if (performance.getEntriesByName(`perf-${name}`).length > 0) {
-    // eslint-disable-next-line no-console
-    console.debug(`perf-${name} already exists`);
-    return;
-  }
   performance.mark(`perf-start-${name}`, detail ? { detail } : undefined);
   // eslint-disable-next-line no-console
   console.debug(`perf-${name} started at ${performance.now()} + ms`);
@@ -35,8 +30,9 @@ window.PerfMarks.measure = (name) => {
 
 const config = {
   attributes: true,
-  subtree: true,
   attributeFilter: ['data-block-status'],
+  attributeOldValue: true,
+  subtree: true,
 };
 
 // const ids = new Map();
