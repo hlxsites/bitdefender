59d831420040eb8d48e8f13c579d3493e0a56860
ltiseanu <ltiseanu@bitdefender.com>
2024-02-19 15:35:07 +0200

    update

--- _src/blocks/slider/slider.js
+++ _src/blocks/slider/slider.js
@@ -1,11 +1,12 @@
 export default function decorate(block) {
+  block.classList.add('default-content-wrapper');
   const parentBlockStyle = block.closest('.section').style;
   const blockStyle = block.style;
   const metaData = block.closest('.section').dataset;
   const {
     type, background_color, text_color, padding_top, padding_bottom, margin_top, margin_bottom
   } = metaData;
-  const allImages = [...block.children];
+  const allSliders = [...block.children];
 
   if (background_color) parentBlockStyle.backgroundColor = background_color;
   if (text_color) blockStyle.color = text_color;
@@ -14,9 +15,14 @@ export default function decorate(block) {
   if (margin_top) blockStyle.marginTop = `${margin_top}rem`;
   if (margin_bottom) blockStyle.marginBottom = `${margimargin_bottomnBottom}rem`;
 
-  block.innerHTML = `
+ block.innerHTML = `
     <div class="container-fluid d-flex justify-between align-center">
-      ${allImages.length ? allImages.map(item => `<div class="logo d-flex align-center">${item.innerHTML}</div>`).join('') : ''}
+      ${allSliders.length ? allSliders.map(item => {
+        const [ sliderText, sliderImage ] = [...item.children];
+        return `<div class="slider d-flex align-center">
+          <div class="slider-text">${sliderText.innerHTML}</div>
+          <div class="slider-image">${sliderImage.innerHTML}</div>`;
+      }).join('') : ''}
     </div>
   `;
 }
