aa3967ac9c7211d0016e34168f607c443752e01c
ltiseanu <ltiseanu@bitdefender.com>
2024-02-20 21:00:05 +0200

    update

---
+++ _src/blocks/custom-block/custom-block.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+  const parentBlockStyle = block.closest('.section').style;
+  const blockStyle = block.style;
+  const metaData = block.closest('.section').dataset;
+  const {
+    contentSize, background_color, text_color, padding_top, padding_bottom, margin_top, margin_bottom
+  } = metaData;
+  const allImages = [...block.children];
+
+  if (background_color) parentBlockStyle.backgroundColor = background_color;
+  if (text_color) blockStyle.color = text_color;
+  if (padding_top) blockStyle.paddingTop = `${padding_top}rem`;
+  if (padding_bottom) blockStyle.paddingBottom = `${padding_bottom}rem`;
+  if (margin_top) blockStyle.marginTop = `${margin_top}rem`;
+  if (margin_bottom) blockStyle.marginBottom = `${margimargin_bottomnBottom}rem`;
+}
