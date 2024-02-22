1c8e7e51e36f7017bf4c6f3ef839c5d5152bcb73
ext-vradulescu <ext-vradulescu@bitdefender.com>
2024-02-23 00:15:37 +0200

    finishes

--- _src/blocks/dual-teaser/dual-teaser.js
+++ _src/blocks/dual-teaser/dual-teaser.js
@@ -1,63 +1,30 @@
-import { isView } from '../../scripts/scripts.js';
-
 export default async function decorate(block) {
   const cols = [...block.children[0].children];
-  let isMobileView = isView('mobile');
 
-  function render() {
-    block.innerHTML = `
-    <div class="container">
-        <div class="col-container">
-            ${cols.map((col) => {
+  block.innerHTML = `
+    <div class="default-content-wrapper">
+        ${cols.map((col) => {
     const pictureEl = col.querySelector('picture');
     const richTextEls = [...col.children];
     richTextEls.shift();
+    const [titleEl, subtitleEl, buttonEl, offerEl] = richTextEls;
 
     return `
-          <div class="col">
-              <div class="img-container">
-                ${pictureEl.outerHTML}
-              </div>
-             
-             ${isMobileView ? `<div class="default-content-wrapper d-flex">
-                ${richTextEls.map((item) => item.outerHTML).join('')}
-             </div>` : ''}
-             
-          </div>
-        `;
-  }).join('')}
+        <div class="col-container">
+            <div class="card">
+                <div class="img-container">
+                    ${pictureEl.outerHTML}
+                </div>
+                <div class="box">
+                    ${titleEl.outerHTML}
+                    <div>${subtitleEl.innerHTML}</div>
+                    ${buttonEl.outerHTML}
+                </div>
+            </div>
+            
+            <div class="offer">${offerEl.outerHTML}</div>
         </div>
-        
-        ${!isMobileView ? `<div class="centered-wrapper default-content-wrapper d-flex">
-          ${cols.map((col) => {
-    const richTextEls = [...col.children];
-    richTextEls.shift();
-
-    return `
-      <div class="col">
-          ${richTextEls.map((item) => item.outerHTML).join('')}
-      </div>
-                `;
-  }).join('')}
-</div>` : ''}
-    </div>
+    `;
+  }).join('')}   
   `;
-
-    const resizeObserver = new ResizeObserver(() => {
-      const newViewportView = isView('mobile');
-
-      const viewHasChanged = isMobileView !== newViewportView;
-
-      if (viewHasChanged) {
-        isMobileView = newViewportView;
-
-        render();
-      }
-    });
-
-    resizeObserver.observe(block);
-  }
-
-  render();
 }
-
