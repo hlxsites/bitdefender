bc1dd4b2462cf5990a60166030074da5fa199f3c
ext-vradulescu <ext-vradulescu@bitdefender.com>
2024-02-23 02:03:33 +0200

    finishes

--- _src/scripts/template-factories/trusted.js
+++ _src/scripts/template-factories/trusted.js
@@ -1,54 +1,92 @@
-import { tsParticles } from 'https://cdn.jsdelivr.net/npm/@tsparticles/engine@3.1.0/+esm';
-import { loadAll } from 'https://cdn.jsdelivr.net/npm/@tsparticles/all@3.1.0/+esm';
+import { isView } from '../scripts.js';
+import { debounce } from '../utils/utils.js';
 
-const particleIdSelector = 'particles-js';
+(async () => {
+  let tsParticles;
+  let loadAll;
+  async function init() {
+    if (isView('mobile')) {
+      return;
+    }
 
-const particleDiv = document.createElement('div');
-particleDiv.setAttribute('id', particleIdSelector);
+    tsParticles = (await import('https://cdn.jsdelivr.net/npm/@tsparticles/engine@3.1.0/+esm')).tsParticles;
+    loadAll = (await import('https://cdn.jsdelivr.net/npm/@tsparticles/all@3.1.0/+esm')).loadAll;
 
-document.body.prepend(particleDiv);
+    const particleIdSelector = 'particles-js';
 
-async function loadParticles(options) {
-  await loadAll(tsParticles);
+    const particleDiv = document.createElement('div');
+    particleDiv.setAttribute('id', particleIdSelector);
 
-  await tsParticles.load({ id: particleIdSelector, options });
-}
+    document.body.prepend(particleDiv);
 
-const configs = {
-  particles: {
-    number: {
-      value: 100,
-    },
-    color: {
-      value: '#ffffff',
-    },
-    links: {
-      enable: true,
-      distance: 200,
-    },
-    shape: {
-      type: 'circle',
-    },
-    opacity: {
-      value: 1,
-    },
-    size: {
-      value: {
-        min: 4,
-        max: 6,
+    async function loadParticles(options) {
+      await loadAll(tsParticles);
+
+      await tsParticles.load({ id: particleIdSelector, options });
+    }
+
+    const configs = {
+      particles: {
+        number: {
+          value: 100,
+        },
+        color: {
+          value: '#ffffff',
+        },
+        links: {
+          enable: true,
+          distance: 200,
+        },
+        shape: {
+          type: 'circle',
+        },
+        opacity: {
+          value: 1,
+        },
+        size: {
+          value: {
+            min: 4,
+            max: 6,
+          },
+        },
+        move: {
+          enable: true,
+          speed: 2,
+        },
       },
-    },
-    move: {
-      enable: true,
-      speed: 2,
-    },
-  },
-  background: {
-    color: '#016DFF',
-  },
-  poisson: {
-    enable: true,
-  },
-};
+      background: {
+        color: '#016DFF',
+      },
+      poisson: {
+        enable: true,
+      },
+    };
 
-loadParticles(configs);
+    loadParticles(configs);
+  }
+
+  async function checkForMobile() {
+    const isMobileView = isView('mobile');
+    if (isMobileView && (!tsParticles && !loadAll)) {
+      return;
+    }
+
+    if (isMobileView && tsParticles) {
+      const particles = tsParticles.domItem(0);
+      particles.pause();
+      return;
+    }
+
+    if (!isMobileView && (!tsParticles && !loadAll)) {
+      await init();
+      return;
+    }
+
+    const particles = tsParticles.domItem(0);
+    particles.play();
+  }
+
+  await init();
+
+  window.addEventListener('resize', debounce(checkForMobile, 250));
+})();
