f506afc856a2cb03611f5269bf79ef3631923a63
ext-vradulescu <ext-vradulescu@bitdefender.com>
2024-02-26 14:41:14 +0200

    teaser-logos and big-teaser-section

---
+++ _src/blocks/big-teaser-section/big-teaser-section.js
@@ -0,0 +1,20 @@
+export default async function decorate(block) {
+  console.log(block);
+  const [richTextEl, firstPictureEl, secondPictureEl] = [...block.children];
+
+  block.innerHTML = `
+    <div class="wrapper default-content-wrapper">
+        <div class="rte">${richTextEl.children[0].innerHTML}</div>
+    </div>
+    
+    <div class="imgs-wrapper">
+        <div class="main-img img-container">
+            ${firstPictureEl.querySelector('picture').innerHTML}
+        </div>
+        
+        <div class="second-img img-container">
+            ${secondPictureEl.querySelector('picture').innerHTML}
+        </div>
+    </div>
+  `;
+}
