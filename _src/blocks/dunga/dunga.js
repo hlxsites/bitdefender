0c01eac1eececceec085ffbd87f3661276c7545d
miordache <matei.iordache21@gmail.com>
2024-02-27 17:31:55 +0200

    Refactor dunga.js to improve performance and fix event dispatching

--- _src/blocks/dunga/dunga.js
+++ _src/blocks/dunga/dunga.js
@@ -1,16 +1,16 @@
 export default function decorate(block) {
   setTimeout(() => {
-    window.dispatchEvent(new CustomEvent('shadowDomLoaded'), {
-      bubbles: true,
-      composed: true,
-    });
-  }, 1000);
-
-  setTimeout(() => {
     const element = block.querySelector('.dunga');
     const elementLink = block.querySelector('.dunga a');
     element.style.backgroundColor = '#E4F2FF';
     element.style.color = '#006EFF';
     elementLink.style.color = '#006EFF';
   }, 2000);
+
+  setTimeout(() => {
+    window.dispatchEvent(new CustomEvent('shadowDomLoaded'), {
+      bubbles: true,
+      composed: true,
+    });
+  }, 1000);
 }
