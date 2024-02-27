b801af5a703629ae331738b37180c5bc8cff3101
miordache <matei.iordache21@gmail.com>
2024-02-27 17:08:44 +0200

    Add transition effect to anchor tags and update styling in Dunga block

--- _src/blocks/dunga/dunga.js
+++ _src/blocks/dunga/dunga.js
@@ -1,3 +1,9 @@
 export default function decorate(block) {
-    
+  setTimeout(() => {
+    const element = document.querySelector('.dunga');
+    const elementLink = document.querySelector('.dunga a');
+    element.style.backgroundColor = '#E4F2FF';
+    element.style.color = '#006EFF';
+    elementLink.style.color = '#006EFF';
+  }, 1000);
 }
