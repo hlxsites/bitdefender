94ea32801b36177b6693bede8a3a9c49c0f29fd9
ext-vradulescu <ext-vradulescu@bitdefender.com>
2024-02-28 17:19:11 +0200

    general fixes

--- _src/blocks/custom-block/custom-block.js
+++ _src/blocks/custom-block/custom-block.js
@@ -1,16 +1,20 @@
 export default function decorate(block) {
+  const buttonEl = block.children[0].children[0].lastElementChild;
+  const newDiv = document.createElement('div');
+  newDiv.append(buttonEl);
+  block.append(newDiv);
+
   const parentBlockStyle = block.closest('.section').style;
   const blockStyle = block.style;
   const metaData = block.closest('.section').dataset;
   const {
-    contentSize, background_color, text_color, padding_top, padding_bottom, margin_top, margin_bottom
+    background_color, text_color, padding_top, padding_bottom, margin_top, margin_bottom,
   } = metaData;
-  const allImages = [...block.children];
 
   if (background_color) parentBlockStyle.backgroundColor = background_color;
   if (text_color) blockStyle.color = text_color;
   if (padding_top) blockStyle.paddingTop = `${padding_top}rem`;
   if (padding_bottom) blockStyle.paddingBottom = `${padding_bottom}rem`;
   if (margin_top) blockStyle.marginTop = `${margin_top}rem`;
-  if (margin_bottom) blockStyle.marginBottom = `${margimargin_bottomnBottom}rem`;
+  if (margin_bottom) blockStyle.marginBottom = `${margin_bottom}rem`;
 }
