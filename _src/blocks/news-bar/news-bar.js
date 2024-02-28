6702d852d090fc608cb9abe532128e62a4f2f2aa
miordache <matei.iordache21@gmail.com>
2024-02-28 11:52:15 +0200

    rename component to news-bar

--- _src/blocks/news-bar/news-bar.js
+++ _src/blocks/news-bar/news-bar.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   setTimeout(() => {
-    const element = block.querySelector('.dunga');
-    const elementLink = block.querySelector('.dunga a');
+    const element = block.querySelector('.news-bar');
+    const elementLink = block.querySelector('.news-bar a');
     element.style.backgroundColor = '#E4F2FF';
     element.style.color = '#006EFF';
     elementLink.style.color = '#006EFF';
