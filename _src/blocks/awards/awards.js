0f209f282ff48e985522ae04ba9c29b4869d4425
andserban <andserban@bitdefender.com>
2024-02-29 13:33:11 +0200

    Comparison component

--- _src/blocks/awards/awards.js
+++ _src/blocks/awards/awards.js
@@ -1,9 +1,1 @@
-export default function decorate(block) {
-  const parentSelector = block.closest(".section");
-  const metaData = parentSelector.dataset;
-  const [texte, logouri] = block.children;
-  console.log(metaData.pipi);
-  console.log(logouri);
-  console.log(texte);
-  console.log(parentSelector);
-}
+
