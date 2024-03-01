334a24e7e24753b62d13c33ded0b58fcdaf9ab43
ext-vradulescu <ext-vradulescu@bitdefender.com>
2024-03-01 13:12:26 +0200

    removed particles + our vision first block

--- _src/scripts/template-factories/trusted.js
+++ _src/scripts/template-factories/trusted.js
@@ -1,93 +1,93 @@
-import { isView } from '../scripts.js';
-import { debounce } from '../utils/utils.js';
-
-// init logic to avoid big layout shifts
-setTimeout(async () => {
-  let tsParticles;
-  let loadAll;
-  async function init() {
-    if (isView('mobile')) {
-      return;
-    }
-
-    tsParticles = (await import('https://cdn.jsdelivr.net/npm/@tsparticles/engine@3.1.0/+esm')).tsParticles;
-    loadAll = (await import('https://cdn.jsdelivr.net/npm/@tsparticles/all@3.1.0/+esm')).loadAll;
-
-    const particleIdSelector = 'particles-js';
-
-    const particleDiv = document.createElement('div');
-    particleDiv.setAttribute('id', particleIdSelector);
-
-    document.body.prepend(particleDiv);
-
-    async function loadParticles(options) {
-      await loadAll(tsParticles);
-
-      await tsParticles.load({ id: particleIdSelector, options });
-    }
-
-    const configs = {
-      particles: {
-        number: {
-          value: 130,
-        },
-        color: {
-          value: '#ffffff',
-        },
-        links: {
-          enable: true,
-          distance: 200,
-        },
-        shape: {
-          type: 'circle',
-        },
-        opacity: {
-          value: 0.6,
-        },
-        size: {
-          value: {
-            min: 2,
-            max: 4,
-          },
-        },
-        move: {
-          enable: true,
-          speed: 2,
-        },
-      },
-      background: {
-        color: '#016DFF',
-      },
-      poisson: {
-        enable: true,
-      },
-    };
-
-    loadParticles(configs);
-  }
-
-  async function checkForMobile() {
-    const isMobileView = isView('mobile');
-    if (isMobileView && (!tsParticles && !loadAll)) {
-      return;
-    }
-
-    if (isMobileView && tsParticles) {
-      const particles = tsParticles.domItem(0);
-      particles.pause();
-      return;
-    }
-
-    if (!isMobileView && (!tsParticles && !loadAll)) {
-      await init();
-      return;
-    }
-
-    const particles = tsParticles.domItem(0);
-    particles.play();
-  }
-
-  await init();
-
-  window.addEventListener('resize', debounce(checkForMobile, 250));
-}, 3000);
+// import { isView } from '../scripts.js';
+// import { debounce } from '../utils/utils.js';
+//
+// // init logic to avoid big layout shifts
+// setTimeout(async () => {
+//   let tsParticles;
+//   let loadAll;
+//   async function init() {
+//     if (isView('mobile')) {
+//       return;
+//     }
+//
+//     tsParticles = (await import('https://cdn.jsdelivr.net/npm/@tsparticles/engine@3.1.0/+esm')).tsParticles;
+//     loadAll = (await import('https://cdn.jsdelivr.net/npm/@tsparticles/all@3.1.0/+esm')).loadAll;
+//
+//     const particleIdSelector = 'particles-js';
+//
+//     const particleDiv = document.createElement('div');
+//     particleDiv.setAttribute('id', particleIdSelector);
+//
+//     document.body.prepend(particleDiv);
+//
+//     async function loadParticles(options) {
+//       await loadAll(tsParticles);
+//
+//       await tsParticles.load({ id: particleIdSelector, options });
+//     }
+//
+//     const configs = {
+//       particles: {
+//         number: {
+//           value: 130,
+//         },
+//         color: {
+//           value: '#ffffff',
+//         },
+//         links: {
+//           enable: true,
+//           distance: 200,
+//         },
+//         shape: {
+//           type: 'circle',
+//         },
+//         opacity: {
+//           value: 0.6,
+//         },
+//         size: {
+//           value: {
+//             min: 2,
+//             max: 4,
+//           },
+//         },
+//         move: {
+//           enable: true,
+//           speed: 2,
+//         },
+//       },
+//       background: {
+//         color: '#016DFF',
+//       },
+//       poisson: {
+//         enable: true,
+//       },
+//     };
+//
+//     loadParticles(configs);
+//   }
+//
+//   async function checkForMobile() {
+//     const isMobileView = isView('mobile');
+//     if (isMobileView && (!tsParticles && !loadAll)) {
+//       return;
+//     }
+//
+//     if (isMobileView && tsParticles) {
+//       const particles = tsParticles.domItem(0);
+//       particles.pause();
+//       return;
+//     }
+//
+//     if (!isMobileView && (!tsParticles && !loadAll)) {
+//       await init();
+//       return;
+//     }
+//
+//     const particles = tsParticles.domItem(0);
+//     particles.play();
+//   }
+//
+//   await init();
+//
+//   window.addEventListener('resize', debounce(checkForMobile, 250));
+// }, 3000);
