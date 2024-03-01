8bc9ab42d62ae6ad133d5fe0a192f4344109e52b
Ionut Mantescu <imantescu@bitdefender.com>
2024-03-01 15:50:08 +0200

    fix lint dlp components

--- _src/blocks/four-cards/four-cards.js
+++ _src/blocks/four-cards/four-cards.js
@@ -1,14 +1,14 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
-    const ul = document.createElement('ul');
+  const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
-    
+
     ul.append(li);
   });
-  
+
   block.textContent = '';
   block.append(ul);
   decorateIcons(block);
