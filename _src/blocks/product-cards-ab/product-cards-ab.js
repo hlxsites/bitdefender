dbad61a84f90a11ca7e0b49df1c6318ffc33ab20
Matei Iordache <matei.iordache21@gmail.com>
2024-03-25 23:29:22 +0200

    Lint fixes

--- _src/blocks/product-cards-ab/product-cards-ab.js
+++ _src/blocks/product-cards-ab/product-cards-ab.js
@@ -6,10 +6,10 @@
 /* eslint-disable max-len */
 export default function decorate(block, options) {
   const {
-    // eslint-disable-next-line no-unused-vars
     pid,
   } = options ? options.metadata : block.closest('.section').dataset;
   if (options) {
+    // eslint-disable-next-line no-param-reassign
     block = block.querySelector('.block');
     let blockParent = block.closest('.section');
     blockParent.classList.add('we-container');
@@ -93,7 +93,6 @@ export default function decorate(block, options) {
 
       fetchProduct(prodName, `${prodUsers}u-${prodYears}y`, pid)
         .then((product) => {
-          console.log(product);
           discountPercentage = Math.round(
             (1 - (product.discount.discounted_price) / product.price) * 100,
           );
