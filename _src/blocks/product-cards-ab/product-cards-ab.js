e9f3a782953594f4de2992873006bb28a863e7eb
Matei Iordache <matei.iordache21@gmail.com>
2024-03-29 12:53:54 +0200

    Remove unnecessary event in product-cards-ab.js

--- _src/blocks/product-cards-ab/product-cards-ab.js
+++ _src/blocks/product-cards-ab/product-cards-ab.js
@@ -196,7 +196,6 @@ export default function decorate(block, options) {
   if (options) {
     window.addEventListener('codeBaseFinishedRunning', () => {
       window.adobeDataLayer.push({
-        event: 'product loaded',
         product: 0,
       });
 
