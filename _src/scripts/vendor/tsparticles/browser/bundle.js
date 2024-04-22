49ca8103e61f88d0ed5d691a12c254effa868db3
Matei Iordache <matei.iordache21@gmail.com>
2024-04-22 15:16:17 +0300

    Remove unused tsParticles files

--- _src/scripts/vendor/tsparticles/browser/bundle.js
+++
@@ -1,6 +0,0 @@
-import { loadFull } from ".";
-import { tsParticles } from "@tsparticles/engine";
-void loadFull(tsParticles);
-export { loadFull };
-export * from "@tsparticles/slim";
-export * from "@tsparticles/engine";
