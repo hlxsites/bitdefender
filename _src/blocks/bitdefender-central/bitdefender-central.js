2b7326a4b0d11fb10ed669d8b524d4f512ca1c96
Matei Iordache <matei.iordache21@gmail.com>
2024-04-24 14:57:32 +0300

    Refactor bitdefender-central.js to remove unused code and update decorations

--- _src/blocks/bitdefender-central/bitdefender-central.js
+++ _src/blocks/bitdefender-central/bitdefender-central.js
@@ -161,8 +161,6 @@ export default async function decorate(block, options) {
   if (options) {
     // eslint-disable-next-line no-param-reassign
     block = block.querySelector('.block');
-    const blockParent = block.closest('.section');
-    blockParent.classList.add('we-container');
     decorateLinkedPictures(block);
     decorateButtons(block);
 
