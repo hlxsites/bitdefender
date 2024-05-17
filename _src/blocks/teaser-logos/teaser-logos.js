63233954e6b4f5ba32b1bd7682b006a2cae67519
ext-vradulescu <ext-vradulescu@bitdefender.com>
2024-05-17 19:25:24 +0300

    cleanup

--- _src/blocks/teaser-logos/teaser-logos.js
+++ _src/blocks/teaser-logos/teaser-logos.js
@@ -1,27 +1,11 @@
-import { getDatasetFromSection } from '../../scripts/utils/utils.js';
-
 export default async function decorate(block) {
-  const blockDataset = getDatasetFromSection(block);
-  const backgrounds = blockDataset.backgrounds.split(',');
-
   const boxes = [...block.children[0].children];
 
   block.innerHTML = `
-    <div class="main-wrapper">
-        ${boxes.map(() => `
-          <div class="col"></div>
-        `).join('')}
-    </div>
     <div class="default-content-wrapper">
         ${boxes.map((box) => `
           <div class="col"><div class="img-wrapper">${box.innerHTML}</div></div>
         `).join('')}
     </div>
   `;
-
-  const cols = [...block.querySelectorAll('.col')];
-
-  backgrounds.forEach((bg, index) => {
-    cols[index].style.setProperty('--backgroundColor', bg);
-  });
 }
