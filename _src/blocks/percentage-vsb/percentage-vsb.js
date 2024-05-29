c9039d1b9d018ee17c1c62262d3f0cbf116882e8
andserban <andserban@bitdefender.com>
2024-05-29 15:26:14 +0300

    new component

---
+++ _src/blocks/percentage-vsb/percentage-vsb.js
@@ -0,0 +1,44 @@
+export default function decorate(block,options) {
+  if (options) {
+    // eslint-disable-next-line no-param-reassign
+    block = block.querySelector('.block');
+    const blockParent = block.closest('.section');
+    blockParent.classList.add('we-container');
+  }
+
+    const parentSelector = block.closest('.section');
+    const [title, subtitle, button, colTitle1, colText1, colTitle2, colText2, colTitle3, colText3 ] = block.children;
+    //create the structure
+    title.classList.add('title-class');
+    subtitle.classList.add('subtitle-class');
+    const columnsContainer = document.createElement('div');
+    columnsContainer.classList.add("columns-container");
+    
+    const column1 = document.createElement('div');
+    column1.appendChild(colTitle1);
+    column1.appendChild(colText1);
+    columnsContainer.appendChild(column1);
+    column1.classList.add('col');
+
+    const column2 = document.createElement('div');
+    column2.appendChild(colTitle2);
+    column2.appendChild(colText2);
+    columnsContainer.appendChild(column2);
+    column2.classList.add('col');
+
+    const column3 = document.createElement('div');
+    column3.appendChild(colTitle3);
+    column3.appendChild(colText3);
+    columnsContainer.appendChild(column3);
+    column3.classList.add('col');
+
+    block.appendChild(columnsContainer);
+
+    block.appendChild(button);
+
+
+  window.dispatchEvent(new CustomEvent('shadowDomLoaded'), {
+    bubbles: true,
+    composed: true, // This allows the event to cross the shadow DOM boundary
+  });
+}
