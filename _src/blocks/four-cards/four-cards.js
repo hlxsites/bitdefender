a59109ef3a829d02885e243013c064a12392384f
Matei Iordache <matei.iordache21@gmail.com>
2024-05-31 15:33:55 +0300

    Update margintop option in four-cards.js

--- _src/blocks/four-cards/four-cards.js
+++ _src/blocks/four-cards/four-cards.js
@@ -1,9 +1,19 @@
 export default function decorate(block, options) {
+  const {
+    // eslint-disable-next-line no-unused-vars
+    margintop,
+  } = options ? options.metadata : block.closest('.section').dataset;
+
   if (options) {
     // eslint-disable-next-line no-param-reassign
     block = block.querySelector('.block');
     const blockParent = block.closest('.section');
     blockParent.classList.add('we-container');
+  }
+
+  if (margintop) {
+    const blockParent = block.closest('.section');
+    blockParent.style.marginTop = `${margintop}px`;
   }
 
   const ul = document.createElement('ul');
