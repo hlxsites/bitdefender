a2f4af14328cab465b43efce976588e43b1c0d23
vradulescu-bd <140399829+vradulescu-bd@users.noreply.github.com>
2024-06-05 14:06:59 +0300

    [DEX-18110] - Design fixes for free page (#488)

--- solutions/blocks/image-columns/image-columns.js
+++ solutions/blocks/image-columns/image-columns.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  block.parentElement.classList.add('default-content-wrapper');
 
   // setup image columns
   [...block.children].forEach((row) => {
